a401f0c18bfd0fbe2092df33885560ccc264b207
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2014-07-24 13:50:06 -0500

    Automatically generating dart/html files needed for headless running.

---
+++ lib/src/testMainHeadless.dart
@@ -0,0 +1,13 @@
+import 'package:unittest/html_config.dart';
+import 'testMain.dart' as testMain;
+
+/**
+* THIS IS A GENERATED FILE. DO NOT MODIFY OR COMMIT.  PLEASE ADD ME TO YOUR .gitignore OR .hgignore
+* Brought to you buy grinder_automated_test_task
+*/
+
+void main() {
+  useHtmlConfiguration();
+
+  testMain.main();
+}
